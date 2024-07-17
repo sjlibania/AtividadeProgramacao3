fd504feaa5c17a43ce1ef793df4585f0cf2210ca
sjlibania <166406038+sjlibania@users.noreply.github.com>
2024-07-17 15:57:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            /* do/while
+             
+            do
+            (    
+                 Console.Write("Digite número Positivo:  ");
+                 a = int.Parse(Console.ReadLine());
+
+            )
+            while (a <= 0);
+            Console.WriteLine("O número digitado foi (0)", a);
+            */
+
+            a = 0;
+            while (a <= 0)
+            {
+                Console.Write("Digite numero positivo: ");
+                a = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("O número digitado foi {0}", a);
+
+
+
+
+
+        }
+    }
+}
